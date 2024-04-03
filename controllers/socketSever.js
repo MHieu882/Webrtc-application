4146ec51a94da4d744b80c59d6c4d76253f56f05
Lê Minh Hiếu <84466517+MHieu882@users.noreply.github.com>
2024-04-03 06:36:48 +0000

    fix delete

--- controllers/socketSever.js
+++ controllers/socketSever.js
@@ -1,8 +1,8 @@
 const activeUsers = {};
 const Message = require("../models/MessageModel");
-const MessageGroup=require('../models/MessGroup')
+const MessageGroup = require("../models/MessGroup");
 const User = require("../models/userModel");
-const Group= require('../models/GroupModel')
+const Group = require("../models/GroupModel");
 const handleSocketEvents = (socket) => {
   socket.on("Login", (userLoggin) => {
     activeUsers[userLoggin] = socket.id;
@@ -20,78 +20,88 @@ const handleSocketEvents = (socket) => {
   });
   socket.on("sendMessage", async (data) => {
     const targetSocketId = activeUsers[data.usertarget];
-    const send=await User.findOne({ username: data.userLoggin });
-    const recei=await User.findOne({ username: data.usertarget });
+    const send = await User.findOne({ username: data.userLoggin });
+    const recei = await User.findOne({ username: data.usertarget });
     //neu targett la  gr
-    const group=await Group.findOne({ Groupname: data.usertarget }).populate('members');
-    if(!recei){
+    const group = await Group.findOne({ Groupname: data.usertarget }).populate(
+      "members",
+    );
+    if (!recei) {
       const message = new MessageGroup({
         sender: send._id,
         receiver: group._id,
         content: data.message,
-      })
-      await message.populate(['sender','receiver'])
-      await message.save();
-      group.members.forEach(member => {
-        const targetSocketId = activeUsers[member.username];
-      socket.to(targetSocketId).emit("greceive", { message });
       });
-    }else{
+      await message.populate(["sender", "receiver"]);
+      await message.save();
+      group.members.forEach((member) => {
+        const targetSocketId = activeUsers[member.username];
+        socket.to(targetSocketId).emit("greceive", { message });
+      });
+    } else {
       const message = new Message({
         sender: send._id,
         receiver: recei._id,
         content: data.message,
       });
-      await message.populate(['sender','receiver'])
+      await message.populate(["sender", "receiver"]);
       await message.save();
       socket.to(targetSocketId).emit("receiveMessage", { message });
     }
-   
   });
   socket.on("getMessage", async (data) => {
     const { targetUser } = data;
-    
+
     const getavt = await User.findOne({ username: targetUser });
-    if(getavt){
+    if (getavt) {
       //user
       const messages = await Message.find(
         {
           $or: [{ receiver: getavt._id }, { sender: getavt._id }],
         },
         { sender: 1, content: 1, receiver: 1, _id: 0 }, // Chỉ lấy trường sender và content, bỏ qua trường _id
-      ).sort({ timestamp: 1 }).populate(['sender','receiver']);
-      socket.emit("loadMess", { messages, targetUser,avt:getavt.avatar}); 
-    }
-    else{
+      )
+        .sort({ timestamp: 1 })
+        .populate(["sender", "receiver"]);
+      socket.emit("loadMess", { messages, targetUser, avt: getavt.avatar });
+    } else {
       //group
-      const group=await Group.findOne({Groupname:targetUser})
-      const messages = await MessageGroup.find({ receiver: group._id }).sort({ timestamp: 1 }).populate(['sender','receiver']);
-      socket.emit("loadmessGroup", { messages, targetUser,avt:group.avatar}); 
+      const group = await Group.findOne({ Groupname: targetUser });
+      const messages = await MessageGroup.find({ receiver: group._id })
+        .sort({ timestamp: 1 })
+        .populate(["sender", "receiver"]);
+      socket.emit("loadmessGroup", { messages, targetUser, avt: group.avatar });
     }
-   
   });
   socket.on("deleteMessage", async (data) => {
     //user
     const { targetUser, userLoggin } = data;
-    const Login=  await User.findOne({ username: userLoggin });
-    const target=  await User.findOne({ username: targetUser });
-    await Message.deleteMany({ sender: Login._id, receiver: target._id });
-    const messages = await Message.find(
-      {
-        $or: [{ receiver: Login._id }, { sender: target._id }],
-      },
-      { sender: 1, content: 1, receiver: 1, _id: 0 }, // Chỉ lấy trường sender và content, bỏ qua trường _id
-    ).sort({ timestamp: 1 }).populate(['sender','receiver']);;
-    socket.emit("loadMess", { messages, targetUser });
-  if(!targettarget){
+    const Login = await User.findOne({ username: userLoggin });
+    const target = await User.findOne({ username: targetUser });
 
-  }else{
-    const target=  await Group.findOne({ Groupname: targetUser });
-    await MessageGroup.deleteMany({ sender: Login._id, receiver: target._id });
-    const group=await Group.findOne({Groupname:targetUser})
-    const messages = await MessageGroup.find({ receiver: group._id }).sort({ timestamp: 1 }).populate(['sender','receiver']);
-    socket.emit("loadmessGroup", { messages, targetUser,avt:group.avatar}); 
-  }
+    if (!target) {
+      const target = await Group.findOne({ Groupname: targetUser });
+      await MessageGroup.deleteMany({
+        sender: Login._id,
+        receiver: target._id,
+      });
+      const group = await Group.findOne({ Groupname: targetUser });
+      const messages = await MessageGroup.find({ receiver: group._id })
+        .sort({ timestamp: 1 })
+        .populate(["sender", "receiver"]);
+      socket.emit("loadmessGroup", { messages, targetUser, avt: group.avatar });
+    } else {
+      await Message.deleteMany({ sender: Login._id, receiver: target._id });
+      const messages = await Message.find(
+        {
+          $or: [{ receiver: Login._id }, { sender: target._id }],
+        },
+        { sender: 1, content: 1, receiver: 1, _id: 0 }, // Chỉ lấy trường sender và content, bỏ qua trường _id
+      )
+        .sort({ timestamp: 1 })
+        .populate(["sender", "receiver"]);
+      socket.emit("loadMess", { messages, targetUser });
+    }
   });
   // Handle disconnect
   socket.on("disconnect", () => {
